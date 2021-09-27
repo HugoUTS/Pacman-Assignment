cdf28bb6867fc14bf4676c25c9b3fdd88fbdaa82
HugoUTS <hugo.tien@student.uts.edu.au>
2021-09-27 21:51:16 +1000

    Pacman Moving

--- Pacman/Assets/Scripts/Movement.cs
+++ Pacman/Assets/Scripts/Movement.cs
@@ -3,6 +3,44 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    public float speed = 4.0f;
 
-    public new Rigidbody2D rigidbody { get; private set; }
+    private Vector2 direction = Vector2.zero;
+
+    //To Start
+    void Start()
+    {
+        
+    }
+    // Update per frame
+    void Update()
+    {
+        CheckInput();
+
+        Move ();
+        
+    }
+
+    void CheckInput ()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+
+            direction = Vector2.left;
+
+        else if (Input.GetKeyDown(KeyCode.D))
+            direction = Vector2.right;
+
+        else if (Input.GetKeyDown(KeyCode.W))
+            direction = Vector2.up;
+
+        else if (Input.GetKeyDown(KeyCode.S))
+            direction = Vector2.down;
+    }
+
+    void Move ()
+    {
+        transform.localPosition += (Vector3)(direction * speed) * Time.deltaTime;
+    }
 }
+
+
